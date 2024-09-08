4640e93d20727dfcaa63fcd2ac77941b6128d08e
umay.oit07@gmail.com <177226299+m-oit@users.noreply.github.com>
2024-09-08 05:48:20 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+  
   get 'top' => 'homes#top'
+  resources :lists
   
-  get '/homes/top', to: 'homes#top'
-  get 'lists/:id/edit', to: 'lists#edit', as: 'edit_list'
-
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  
-  end
+end 
