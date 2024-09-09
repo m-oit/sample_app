ebe88ec936e4b3537c1a0469c576091b1ff3e243
umay.oit07@gmail.com <177226299+m-oit@users.noreply.github.com>
2024-09-09 01:37:29 +0000

    [Finish}3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,19 +6,18 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    #@がついていないローカル変数はビューファイルに受け渡しができない
+      @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+    end
     
-     @list = List.new(list_params)
-     
+  
    #上記をList.find(2)にすると投稿後list/2の結果が表示される
    #データをデータベースに保存するためのsaveメソッド実行
-    @list.save
-    
-    redirect_to list_path(@list.id)
-   
-   
-  end
+  
 
   def index
     @lists = List.all  
